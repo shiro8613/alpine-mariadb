bbf7f33d1108c7180658804b89628fc11559f31e
toms0910 <psp1070@gmail.com>
2023-02-17 23:25:13 +0900

    setup

---
+++ 10.11/install.sh
@@ -0,0 +1,38 @@
+#!/bin/ash
+
+#Check root
+echo "Check root..."
+
+#Install tools
+echo "Install tools"
+
+#Install dependencies
+echo "Install dependencies"
+
+#Clone mariadb source
+echo "Clone mariadb source"
+
+#Run cmake
+echo "Run cmake"
+
+#Run make install
+echo "Run make install"
+
+#Get add files
+echo "Get add files"
+
+#Add user
+echo "Add user"
+
+#Ownership change
+echo "Ownership change"
+
+#Init database
+echo "Init database"
+
+#Regist service
+echo "Regist service"
+
+#Run mariadbd
+echo "Run mariadbd"
+
