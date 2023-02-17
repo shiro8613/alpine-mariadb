7568242fb2b663466b2d439bb6816d01b8678ecc
toms0910 <psp1070@gmail.com>
2023-02-18 00:07:35 +0900

    add build

--- 10.11/install.sh
+++ 10.11/install.sh
@@ -5,18 +5,26 @@ echo "Check root..."
 
 #Install tools
 echo "Install tools"
+apk add git gcc g++ make bison ncurses cmake opensll --no-cache
 
 #Install dependencies
 echo "Install dependencies"
+apk add libaio-dev ncurese-dev zlib-dev libevent-dev ncurses-dev libevent zstd-libs libcurl curl-dev zstd zstd-dev --no-cache
+apk add gnutls-dev bzip2-dev lz4-dev lzo-dev snappy-dev xz-dev libxml2-dev boost-dev flex lz4 linux-pam-dev --no-cache
 
 #Clone mariadb source
 echo "Clone mariadb source"
+git clone https://github.com/MariaDB/server.git mariadb
+cd mariadb
+git checkout 10.11
 
 #Run cmake
 echo "Run cmake"
+cmake . -DBUILD_CONFIG=mysql_release
 
 #Run make install
 echo "Run make install"
+make install
 
 #Get add files
 echo "Get add files"
